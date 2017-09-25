1f6f8536e116c4feb4ab1c97c3a6573671721683
vectorzhang <409273291@qq.com>
2017-09-25 19:54:20 +0800

    add module zk study

--- zk-study/src/main/java/com/zxl/app/study/zk/ZkStudy.java
+++ zk-study/src/main/java/com/zxl/app/study/zk/ZkStudy.java
@@ -4,6 +4,7 @@ import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
 
@@ -15,17 +16,61 @@ import java.io.IOException;
  **/
 public class ZkStudy {
 
+    public static final String HOST = "127.0.0.1:2181";
+
     public static final String PATH = "/zk_study";
 
-    public static void main(String[] args) throws InterruptedException, IOException, KeeperException {
-        new ZkStudy().create(PATH, "zxl");
+    private ZooKeeper zkClient;
+
+    public ZkStudy() {
+        try {
+            zkClient = new ZooKeeper(HOST, 1000, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public boolean create(String path, String data) throws IOException, KeeperException, InterruptedException {
-        ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", 1000, null);
-        String result = zk.create(path, data.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-        String dataStr = new String(zk.getData(path, null, null));
+    public static void main(String[] args) throws InterruptedException, IOException, KeeperException {
+        ZkStudy zkStudy = new ZkStudy();
+        zkStudy.create(PATH, "zxl");
+        zkStudy.delete(PATH);
+        zkStudy.set(PATH, "1234");
+    }
+
+
+    public boolean create(String path, String data) {
+        String result = null;
+        try {
+            result = zkClient.create(path, data.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            String dataStr = new String(zkClient.getData(path, null, null));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println(String.format("create data=%s, result=%s", data, result));
+        return true;
+    }
+
+    public boolean set(String path, String data) {
+        try {
+            Stat stat = zkClient.setData(path, data.getBytes(), -1);
+            System.out.println(String.format("set data=%s,result=%s", data, stat));
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public boolean delete(String path) {
+        try {
+            zkClient.delete(path, -1);
+            System.out.println(String.format("delete path=%s", path));
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return true;
     }
 }
