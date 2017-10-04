30ed2e238601603c4bee3fc089f827d2275fe7f7
ceeker <409273291@qq.com>
2017-10-04 20:54:02 +0800

    调整包结构，增加响应式编程学习

--- reactor-study/src/main/java/com/ceeker/app/study/reactor/ReactorSnippets.java
+++ reactor-study/src/main/java/com/ceeker/app/study/reactor/ReactorSnippets.java
@@ -1,11 +1,18 @@
 package com.ceeker.app.study.reactor;
 
+import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
 
 
+/**
+ * https://www.ibm.com/developerworks/cn/java/j-cn-with-reactor-response-encode/index.html
+ * http://www.infoq.com/cn/articles/reactor-by-example
+ */
 public class ReactorSnippets {
     private static List<String> words = Arrays.asList(
             "the",
@@ -19,11 +26,32 @@ public class ReactorSnippets {
             "dog"
     );
 
-    public static void main(String[] args) {
+    @Test
+    public void simpleCreation() {
         Flux<String> fewWords = Flux.just("Hello", "World");
         Flux<String> manyWords = Flux.fromIterable(words);
+
         fewWords.subscribe(System.out::println);
         System.out.println();
         manyWords.subscribe(System.out::println);
+    }
+
+
+    @Test
+    public void findingMissingLetter() {
+        Flux<String> manyLetters = Flux
+                .fromIterable(words)
+                .flatMap(word -> Flux.fromArray(word.split("")))
+                .distinct()
+                .sort()
+                .zipWith(Flux.range(1, Integer.MAX_VALUE),
+                        (string, count) -> String.format("%2d. %s", count, string));
+
+        manyLetters.subscribe(System.out::println);
+        Flux.just("Hello", "World").subscribe(System.out::println);
+        Flux.fromArray(new Integer[]{1, 2, 3}).subscribe(System.out::println);
+        Flux.empty().subscribe(System.out::println);
+        Flux.range(1, 10).subscribe(System.out::println);
+        Flux.interval(Duration.of(10, ChronoUnit.SECONDS)).subscribe(System.out::println);
     }
 }
