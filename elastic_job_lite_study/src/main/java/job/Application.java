9fc18a828e0d58f5b3fc766e12629c087fe6cd33
ceeker <409273291@qq.com>
2017-10-06 16:12:58 +0800

    采用springboot启动job

--- elastic_job_lite_study/src/main/java/job/Application.java
+++ elastic_job_lite_study/src/main/java/job/Application.java
@@ -4,16 +4,24 @@ import com.dangdang.ddframe.job.lite.api.JobScheduler;
 import job.demo.JobDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author vectorzhang
  * @desc
  * @date 2017/10/6 15:57
  */
+@RestController
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         new JobScheduler(JobDemo.createRegistryCenter(), JobDemo.createJobConfiguration()).init();
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "hello world";
     }
 }
